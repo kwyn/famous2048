f00262fcec8dafe4ad824c02169a5757038cdadc
HarleyKwyn <kwyn.meagher@gmail.com>
2014-04-20 13:10:33 -0700

    Adds threshold to sync

--- app/src/AppView.js
+++ app/src/AppView.js
@@ -48,16 +48,18 @@ define(function(require, exports, module) {
         this.pageView.pipe(this.verticalSync);
 
         this.latteralSync.on('update', function(data) {
-            if(Math.abs(data.delta) > 0 )
-                console.log('x = ' +data.delta);
-            this.pageViewPos = data.p;
-            this.pageMod.setTransform(Transform.translate(data.p, 0, 0));
+            if(Math.abs(data.delta) > 10 ) {
+                console.log('x = ' +data.position);
+                this.pageViewX = data.position;
+                this.pageMod.setTransform(Transform.translate(data.position, 0, 0));
+            }
         }.bind(this));
          this.verticalSync.on('update', function(data) {
-            if(Math.abs(data.delta) > 0 )
-                console.log('y = ' + data.delta);
-            this.pageViewPos = data.p;
-            this.pageMod.setTransform(Transform.translate(data.p, 0, 0));
+            if(Math.abs(data.delta) > 10 ){
+                console.log('y = ' + data.position);
+                this.pageViewY = data.position;
+                this.pageMod.setTransform(Transform.translate(0, data.position, 0));
+            }
         }.bind(this));
     }
 
